4553e8c144b7657b9557f4e9dd55fde4581cf305
naitik480 <anjitad770@gmail.com>
2026-02-18 16:04:26 +0530

    Add files via upload

--- angle.cpp
+++ angle.cpp
@@ -5,15 +5,15 @@ int main(){
 
 int third_angle,first,second;
 
-cout << "enter 1st angle :";
+cout << "enter 1st angle of triangle :";
 cin >> first;
 
-cout << "enter 2nd angle :";
+cout << "enter 2nd angle of triangle:";
 cin >> second;
 
 third_angle=180-(first+second);
 
-cout << "third angle =" << third_angle;
+cout << "third angle of triangle =" << third_angle;
 
 return 0;
 
