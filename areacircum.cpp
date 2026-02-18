a1be6f428f1aa5a9abcbfb1952aafccfa8e70706
naitik480 <anjitad770@gmail.com>
2026-02-18 15:48:23 +0530

    Add files via upload

---
+++ areacircum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float area,circumference,r;
+
+    cout << "enter radius of circle :";
+    cin >> r;
+
+    area =3.14159*r*r;
+
+    cout << "area =" << area ;
+
+    circumference =2*3.14159*r;
+
+    cout  <<"\n" "circumference =" << circumference;
+
+
+
+    return 0;
+
+
+
+
+}
