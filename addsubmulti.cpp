85cee5aff36cbe0d5ed89b14870999d7ab4c0908
naitik480 <anjitad770@gmail.com>
2026-02-20 12:44:28 +0530

    Add files via upload

---
+++ addsubmulti.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float num1,num2,add,sub,div,multi;
+
+    cout << "enter 1st no.";
+    cin >> num1;
+
+    cout << "enter 2nd no.";
+    cin >> num2;
+
+    add=num1+num2;
+    sub=num1-num2;
+    div=num1/num2;
+    multi=num1*num2;
+
+    cout << "addition of two numbers =" << add <<"\n" "subtraction of two numbers ="<< sub << "\n""division of two numbers =" << div <<"\n" "multiplication of two numbers ="<< multi;
+
+    return 0;
+
+
+
+
+}
---
+++ celcius.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+float celsius,f;
+
+cout << "enter temperature in celsius :";
+cin >> celsius;
+
+f=(9/5*celsius)+32;
+
+cout << "Fahrenheit =" << f;
+
+return 0;
+}
