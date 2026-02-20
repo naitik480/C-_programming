b72ca6a5d2522410fb5a074708f9f8259aef2faa
naitik480 <anjitad770@gmail.com>
2026-02-20 12:55:22 +0530

    Add files via upload

---
+++ remainder.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ int main(){
+     int num1,num2,remainder;
+     cout << "enter 1st no.:";
+     cin >> num1;
+
+     cout << "enter 2nd no.:";
+     cin >> num2;
+
+     remainder = num1%num2;
+
+     cout << "remainder =" << remainder;
+
+     return 0;
+
+
+
+
+    }
