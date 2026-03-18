2c82556212981e52b53af00d002d104b6ea98ab5
naitik480 <anjitad770@gmail.com>
2026-03-18 15:14:25 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+   int main(){
+
+       int number;
+
+    cout << "enter a number=";
+    cin >> number;
+
+    if(number%2 == 0){
+        cout << "even :" << number;
+
+    } else {
+        cout << "odd :" << number;
+
+    }
+
+   return 0;
+
+
+
+
+   }
