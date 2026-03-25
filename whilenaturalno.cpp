94bc4aafa7c75a22e2f79ed2030b36b4a8f91e93
naitik480 <anjitad770@gmail.com>
2026-03-25 14:32:50 +0530

    Add files via upload

---
+++ whilenaturalno.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    int i=1;
+
+    cout << "enter first n natural number:";
+    cin >> n;
+
+    while(i <= n){
+        cout << i << "/n" ;
+        i++;
+    }
+    return 0;
+
+}
